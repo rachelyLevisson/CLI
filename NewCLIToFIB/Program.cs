e2b7ca8050664b9fd05fd35a638f9304d70c7c8c
לויסון <@DESKTOP-9G3ER3M>
2025-01-15 17:12:25 +0200

    note and sort

--- NewCLIToFIB/Program.cs
+++ NewCLIToFIB/Program.cs
@@ -5,10 +5,16 @@ var optionCommand = new Option<FileInfo>("--output","the path output");
 
 var languageOption = new Option<List<string>>("--language", "the language the code or all");
 
+var noteOptions = new Option<FileAttributes>("--note", "The source code is in the comment in the file");
+
+var sortOption = new Option<SortedList<StreamReader,string>("--sort","Order of copying the code files");
+
 var bundle = new Command("bundle", "bundle code to opposite the single page");
 
 bundle.AddOption(optionCommand);
 bundle.AddOption(languageOption);
+bundle.AddOption(noteOptions);
+bundle.AddOption(sortOption);
 
 bundle.SetHandler((output) =>
 {
