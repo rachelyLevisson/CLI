447d51286b306bcc7b7c7e475fc9a0e0be053eaa
לויסון <@DESKTOP-9G3ER3M>
2025-01-15 12:45:24 +0200

    new project

---
+++ NewCLIToFIB/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.CommandLine;
+
+var bundle = new Command("bundle", "bundle code to opposite the single page");
+
+bundle.SetHandler(() =>
+{
+    Console.WriteLine("succses!!");
+});
+
+var rootCommand = new RootCommand("this opposite many page code to signel code");
+
+rootCommand.AddCommand(bundle);
+
+rootCommand.InvokeAsync(args);
