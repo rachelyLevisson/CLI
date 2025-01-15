83d0dab55dec0563f513617673ad31501cd4b3cf
לויסון <@DESKTOP-9G3ER3M>
2025-01-15 16:56:05 +0200

    language

--- NewCLIToFIB/Program.cs
+++ NewCLIToFIB/Program.cs
@@ -3,9 +3,12 @@ using System.CommandLine;
 
 var optionCommand = new Option<FileInfo>("--output","the path output");
 
+var languageOption = new Option<List<string>>("--language", "the language the code or all");
+
 var bundle = new Command("bundle", "bundle code to opposite the single page");
 
 bundle.AddOption(optionCommand);
+bundle.AddOption(languageOption);
 
 bundle.SetHandler((output) =>
 {
