658ab4343a50a24629a2bb03a687a15897ec5afa
לויסון <@DESKTOP-9G3ER3M>
2025-01-15 14:13:47 +0200

    of --output doesnt

--- NewCLIToFIB/Program.cs
+++ NewCLIToFIB/Program.cs
@@ -1,15 +1,20 @@
 ﻿
 using System.CommandLine;
 
+var optionCommand = new Option<FileInfo>("--output","the path output");
+
 var bundle = new Command("bundle", "bundle code to opposite the single page");
 
-bundle.SetHandler(() =>
+bundle.AddOption(optionCommand);
+
+bundle.SetHandler((output) =>
 {
-    Console.WriteLine("succses!!");
-});
+    File.Create(output.FullName);
+    Console.WriteLine("succsess!!");
+},optionCommand);
 
 var rootCommand = new RootCommand("this opposite many page code to signel code");
 
 rootCommand.AddCommand(bundle);
 
-rootCommand.InvokeAsync(args);
+await rootCommand.InvokeAsync(args);
