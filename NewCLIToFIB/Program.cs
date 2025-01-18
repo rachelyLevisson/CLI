fbd12ecc6d86e51e6bcaa37cc838ce2fc8962d52
לויסון <@DESKTOP-9G3ER3M>
2025-01-18 23:33:23 +0200

    prev the stream

--- NewCLIToFIB/Program.cs
+++ NewCLIToFIB/Program.cs
@@ -1,17 +1,32 @@
 ﻿
 using System.CommandLine;
 
-var outputOption = new Option<FileInfo>("--output", "the path output");
+var outputOption = new Option<FileInfo>(
+ new[] { "--output", "-o" },
+  "the path output"
+    );
 
-var languageOption = new Option<string>("--language", "the language the code or all");
+
+//var languageOption = new Option<string>("--language", "the language the code or all");
+
+var languageOption = new Option<string>(
+    new[] { "--language", "-l" }, // Define both the long and short options
+    "the language the code or all"
+);
 
 var noteOptions = new Option<bool>("--note", "The source code is in the comment in the file");
+
+noteOptions.AddAlias("-n");
 
 var sortOption = new Option<string>("--sort", "Order of copying the code files");
 
 var removeEmptyLinesOption = new Option<bool>("--remove-empty-lines", "Delete empty rows");
 
+removeEmptyLinesOption.AddAlias("-rem");
+
 var authorOption = new Option<string>("--author", "Register the name of the file creator");
+
+authorOption.AddAlias("-a");
 
 var bundle = new Command("bundle", "bundle code to opposite the single page");
 
@@ -28,83 +43,111 @@ foreach (var option in bundle.Options)
     Console.WriteLine(option.Name);
 }
 Console.WriteLine();
-
-
+Console.WriteLine("come!!");
 
 
 
 bundle.SetHandler((output, language, note, remove, autho) =>
 {
-    //האם הקיש בכלל את האפשרות של שפה
-    if (string.IsNullOrEmpty(language))
-    {
-        Console.WriteLine("ERROR: --language option is required!");
-        return;
-    }
+    Console.WriteLine("come to  SetHandler!!");
 
-    // האם הכניס ניתוב כל שהוא אם לא יכניס ערך ברירת מחדל
-    if (string.IsNullOrEmpty(output.ToString()))
+    try
     {
-        Console.WriteLine("ERROR: --output option is required!");
-    }
-    else
-        output = new FileInfo("text.txt");
-
-    //האם השפה שהוקשה נמצאת ברשימה
-    if (language == "all" || Enum.TryParse(language, true, out Elanguage lang))
-    {
-        try
+        //      האם הקיש בכלל את האפשרות של שפה
+        if (string.IsNullOrEmpty(language))
         {
-            File.Create(output.FullName);
-            string[] file = Directory.GetFiles(Environment.CurrentDirectory);
-            foreach (var item in file)
+            Console.WriteLine("ERROR: --language option is required!");
+            return;
+        }
+        //    האם הכניס ניתוב כל שהוא אם לא יכניס ערך ברירת מחדל
+        //if (string.IsNullOrEmpty(output.ToString()))
+        //{
+        //    Console.WriteLine("ERROR: --output option is required!");
+        //    output = new FileInfo("text.txt");
+        //}
+        //      האם השפה שהוקשה נמצאת ברשימה
+        if (language == "all" || Enum.TryParse(language, true, out Elanguage lang))
+        {
+            try
             {
-                File.WriteAllText(output.ToString(),File.ReadAllText(item));
+                File.Create(output.FullName);
+                string[] file = Directory.GetFiles(Environment.CurrentDirectory);
+                Console.WriteLine("lenght: "+file.Length);
+                foreach (var item in file)
+                {
+                    Console.WriteLine(item);
+                }
+                //    האם הקיש את האופציה ומכילה TRUE
+                if (note)
+                {
+                    Console.WriteLine("i enter to note!!");
+                    File.AppendText("the write me😜💥");
+                    Console.WriteLine("i write");
+                    File.AppendText("#" + Environment.CurrentDirectory);
+                }
+                //האם הקיש את האופציה ומכילה TRUE
+                if (remove)
+                {
+                    Console.WriteLine("i enter to remove!!");
+
+                    //delete a empty line!!
+                }
+                if (string.IsNullOrEmpty(autho.ToString()))
+                {
+                    Console.WriteLine("i enter to author!!");
+                    File.OpenWrite("#" + autho.ToString());
+                }
             }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("ERROR: the path invalid! check this.");
+                ex.Data.Clear();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Worng!!");
+            }
+            Console.WriteLine("sucssess!!! yesh! yesh! ♥️🎁💥🍔😜");
         }
-        catch (DirectoryNotFoundException ex)
+        else
         {
-            Console.WriteLine("ERROR: the path invalid! check this.");
-            ex.Data.Clear();
+            Console.WriteLine("ERROR: Only code files of the selected languages!");
         }
-        //האם הקיש את האופציה ומכילה TRUE
-        if (note)
-        {
-            File.OpenWrite("#" + Environment.CurrentDirectory);
-        }
-        //האם הקיש את האופציה ומכילה TRUE
-        if (remove)
-        {
-            //delete a empty line!!
-        }
-        if (string.IsNullOrEmpty(autho.ToString()))
-            File.OpenWrite("#" + autho.ToString());
+        Console.WriteLine("succsess!! 😆😂😃🙌🏻");
     }
-    else
+    catch (NullReferenceException e)
     {
-        Console.WriteLine("ERROR: Only code files of the selected languages!");
+        Console.WriteLine("hihi");
+        Console.WriteLine(e.Message);
     }
-    Console.WriteLine("succsess!!");
+    #region //
+    //  }
+    //catch (IOException e)
+    //{
+    //    Console.WriteLine("wo no vivivivi........🥹😞😭");
+    //}
+    //catch (NullReferenceException ne) { Console.WriteLine("errorr ooooffffff"+ne.Message); }
+    #endregion
 }, outputOption, languageOption, noteOptions, removeEmptyLinesOption, authorOption);
 
-
-
-var create_rspCommand = new Command("create-rsp", "Create a response file with a prepared command");
+#region //
+//var create_rspCommand = new Command("create-rsp", "Create a response file with a prepared command");
 
 //create_rspCommand.SetHandler(() =>
 //{
 //    Console.WriteLine("create_rspCommand new!!!");
 //});
+#endregion
 
 var rootCommand = new RootCommand("this opposite many page code to signel code");
 rootCommand.AddCommand(bundle);
 
 
 //rootCommand.AddCommand(create_rspCommand);
-Console.WriteLine("Arguments count: " + args.Length);
+Console.WriteLine("the Arguments count: " + args.Length);
 foreach (var arg in args)
 {
-    Console.WriteLine("Argument: " + arg);
+    Console.WriteLine("ArgumentTo: " + arg);
 }
-
+Console.WriteLine("finishhhh!!");
 await rootCommand.InvokeAsync(args);
